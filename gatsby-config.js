1b6b737cebf759358ea8d8e1979cf62175dce161
Marco Pedrinazzi <marcopedrinazzi.mp@gmail.com>
2021-11-25 20:54:36 +0100

    first config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-jodie/gatsby-config.js
-    siteTitle: `Jodie`,
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
-    siteHeadline: `Jodie - Gatsby Theme from @lekoarts`,
+    siteTitle: `Trial Team 3D`,
+    siteTitleAlt: `Trial Team 3D`,
+    siteHeadline: `Trial Team 3D`,
     siteUrl: `https://jodie.lekoarts.de`,
-    siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+    siteDescription: `Trial Team 3D`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `Marco Pedrinazzi`,
   },
   plugins: [
     {

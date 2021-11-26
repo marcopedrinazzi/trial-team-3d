616e6f8741432b953f492402a1a41b4cc6ed3e47
Marco Pedrinazzi <marcopedrinazzi.mp@gmail.com>
2021-11-26 18:15:13 +0100

    redirect
    first try

--- src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
+++ src/@lekoarts/gatsby-theme-jodie/utils/modify-grid.js
@@ -1,9 +1,7 @@
 import {
     onlyPages
-    //onlyProjects,
   } from "@lekoarts/gatsby-theme-jodie/src/utils/resolver-templates";
 
 const modifyGrid = (data) => onlyPages(data);
-// const modifyGrid = (data) => onlyProjects(data)
 
 export default modifyGrid

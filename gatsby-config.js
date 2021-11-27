97026ea8890f8eddda54220b074996f881f82147
Marco Pedrinazzi <marcopedrinazzi.mp@gmail.com>
2021-11-27 12:31:05 +0100

    sponsor cover image

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,9 +50,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `jodie - @lekoarts/gatsby-theme-jodie`,
-        short_name: `jodie`,
-        description: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
+        name: `Trial Team 3D`,
+        short_name: `Trial Team 3D`,
+        description: `Trial Team 3D`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
@@ -73,7 +73,6 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-redirect-to`,
     `gatsby-plugin-gatsby-cloud`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,

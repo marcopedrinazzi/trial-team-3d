ffb82909389a564d419c7b1697836585e4eb44db
Marco Pedrinazzi <marcopedrinazzi.mp@gmail.com>
2021-11-30 13:07:08 +0100

    img tag responsive

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
@@ -2,11 +2,7 @@ import * as React from "react"
 import logo3d from "../../../../static/logo3d_sito.png"
 
 const Logo = () => (
-  //<img src={logo3d} alt="Trial Team 3D" />
-  <svg height="93" width="150" viewBox="0 0 400 210">
-  <path d="M150 0 L75 200 L225 200 Z" />
-  Sorry, your browser does not support inline SVG.
-</svg>
+  <img src={logo3d} style="width: 100%; height: auto;" alt="Trial Team 3D" />
 )
 
 export default Logo

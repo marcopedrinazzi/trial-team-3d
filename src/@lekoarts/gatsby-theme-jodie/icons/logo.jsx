acc6146295bf19601b5cbad746ccbbf48050f7ed
Marco Pedrinazzi <marcopedrinazzi.mp@gmail.com>
2021-11-30 13:20:16 +0100

    Fix sintassi img

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import logo3d from "../../../../static/logo3d_sito.png"
 
 const Logo = () => (
-  <img src={logo3d} style="width: 100%; height: auto;" alt="Trial Team 3D" />
+  <img src={logo3d} style={{width: "100%", height: auto}} alt="Trial Team 3D" />
 )
 
 export default Logo
